9fd1e1c1ffccac7844e3cf4897f819115b22f96e
Rick <10963428@uvu.edu>
2022-03-23 14:52:43 -0600

    Added Challenge 2 plus bonuses.

---
+++ Create with Code/Challenge 2/Challenge 2/Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+    private float dogTimer = 0;
+    private float dogInterval = 1.0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        //set the dog timer to count time
+        dogTimer = dogTimer + Time.deltaTime;
+        
+        // On spacebar press, send dog
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (dogTimer >= dogInterval)
+            {
+                Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+                dogTimer = 0;
+            }
+            
+        }
+    }
+}
