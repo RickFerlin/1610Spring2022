3bbee239a700315b6d4fb314b5a5ea703294664b
Rick <10963428@uvu.edu>
2022-03-31 15:36:36 -0600

    Added Challenge 4

---
+++ Create with Code/Challenge 4/Challenge 4/Assets/Challenge 4/Scripts/EnemyX.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyX : MonoBehaviour
+{
+    public float speed = 1.2f;
+    private Rigidbody enemyRb;
+    private GameObject playerGoal;
+
+    private int waveNumber;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        enemyRb = GetComponent<Rigidbody>();
+        playerGoal = GameObject.Find("Player Goal");
+        waveNumber = GameObject.Find("Spawn Manager").GetComponent<SpawnManagerX>().waveCount;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Set enemy direction towards player goal and move there
+        Vector3 lookDirection = (playerGoal.transform.position - transform.position).normalized;
+        enemyRb.AddForce(lookDirection * speed * Time.deltaTime);
+        
+        //The enemy balls get faster now.
+        speed = (waveNumber * .2f) + speed;
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        // If enemy collides with either goal, destroy it
+        if (other.gameObject.name == "Enemy Goal")
+        {
+            Destroy(gameObject);
+        } 
+        else if (other.gameObject.name == "Player Goal")
+        {
+            Destroy(gameObject);
+        }
+
+    }
+
+}
